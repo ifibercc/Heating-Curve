513dbc4e3c697883aef2a12665f02a63a6d8b0b4
Lemoo <ifibercc@gmail.com>
2016-04-28 09:40:49 +0800

    将评价曲线改为两个

--- pro/frecheteDataset.m
+++ pro/frecheteDataset.m
@@ -3,28 +3,37 @@ close all
 %% 加载数据
 data = cell(1, 20);
 for i = 1 : 20
-    data{i} = xlsread(['YongXing/', num2str(i), '.xlsx']);
+    data{i} = xlsread(['WanXing/', num2str(i), '.xlsx']);
 end
 %% 检测异常点
-% 3代表万德庄的出水温度，9代表兴泰里的出水温度，5为室外温度，6为室外日照
+% 3代表万德庄的出水温度，9代表兴泰里的出水温度，5为万德庄室外温度，6为万德庄室外日照
+% 11为兴泰里室外温度，12为兴泰里室外日照
 % isnan先检测出哪些点有空值，然后后再插入前后两个点的平均值
-nullWan  = cell(1, 20);
-nullXing = cell(1, 20);
-nullOut  = cell(1, 20);
-nullSun  = cell(1, 20);
+nullExWan  = cell(1, 20);
+nullExXing = cell(1, 20);
+nullOutWan  = cell(1, 20);
+nullSunWan  = cell(1, 20);
+nullOutXing = cell(1, 20);
+nullSunXing = cell(1, 20);
 % 执行3遍，有连续NaN的情况
 
     for i = 1 : 20
         % 查找出万德庄和兴泰里的空数据序列
-        nullWan{i}  = find(isnan(data{i}(:, 3)));
-        nullXing{i} = find(isnan(data{i}(:, 9)));
-        nullOut{i}  = find(isnan(data{i}(:, 5)));
-        nullSun{i}  = find(isnan(data{i}(:, 6)));
+        nullExWan{i}  = find(isnan(data{i}(:, 3)));
+        nullOutWan{i}  = find(isnan(data{i}(:, 5)));
+        nullSunWan{i}  = find(isnan(data{i}(:, 6)));
+        nullExXing{i} = find(isnan(data{i}(:, 9)));
+        nullOutXing{i} = find(isnan(data{i}(:, 11)));
+        nullSunXing{i} = find(isnan(data{i}(:, 12)));
+
         % 将空数据用前后均值填补
-         data{i}(nullWan{i}, 3)  = (data{i}(nullWan{i} - 2, 3) + data{i}(nullWan{i} + 2, 3)) / 2;
-         data{i}(nullXing{i}, 9) = (data{i}(nullXing{i} - 2, 9) + data{i}(nullXing{i} + 2, 9)) / 2;
-         data{i}(nullOut{i}, 5)  = (data{i}(nullOut{i} - 2, 5) + data{i}(nullOut{i} + 2, 5)) / 2;
-         data{i}(nullSun{i}, 6)  = (data{i}(nullSun{i} - 2, 6) + data{i}(nullSun{i} + 2, 6)) / 2;
+%          data{i}(nullExWan{i}, 3)  = (data{i}(nullExWan{i} - 2, 3) + data{i}(nullExWan{i} + 2, 3)) / 2;
+%          data{i}(nullOutWan{i}, 5)  = (data{i}(nullOutWan{i} - 2, 5) + data{i}(nullOutWan{i} + 2, 5)) / 2;
+%          data{i}(nullSunWan{i}, 6)  = (data{i}(nullSunWan{i} - 2, 6) + data{i}(nullSunWan{i} + 2, 6)) / 2;         
+%          data{i}(nullExXing{i}, 9) = (data{i}(nullExXing{i} - 2, 9) + data{i}(nullExXing{i} + 2, 9)) / 2;
+%          data{i}(nullOutXing{i}, 11) = (data{i}(nullOutXing{i} - 2, 11) + data{i}(nullOutXing{i} + 2, 11)) / 2;
+%          data{i}(nullSunXing{i}, 12) = (data{i}(nullSunXing{i} - 2, 12) + data{i}(nullSunXing{i} + 2, 12)) / 2;
+
     end
 
 %% 数据处理
@@ -35,23 +44,26 @@ xingDistArr = zeros(1, 20);
 for i = 1 : 20
     wanTemp    = data{i}(400:1200, 3);
     xingTemp   = data{i}(400:1200, 9);
-    % 评价曲线，室外温度反转，并加上日照因素
-    judgeTemp = -data{i}(400:1200, 5) + 2.5 * (data{i}(400:1200, 6) / 350);
+    % 万德庄评价曲线，室外温度反转，并加上日照因素
+    judgeTempWan = -data{i}(400:1200, 5) + 2.5 * (data{i}(400:1200, 6) / 350);
+    judgeTempXing = -data{i}(400:1200, 11) + 2.5 * (data{i}(400:1200, 12) / 350);
     % 获得三条曲线的至高至低点
     [~, wanMax] = max(wanTemp);
     [~, wanMin] = min(wanTemp);
-    [~, judgeMax] = max(judgeTemp);
-    [~, judgeMin] = min(judgeTemp);
+    [~, judgeMaxWan] = max(judgeTempWan);
+    [~, judgeMinWan] = min(judgeTempWan);
+    [~, judgeMaxXing] = max(judgeTempXing);
+    [~, judgeMinXing] = min(judgeTempXing);
     [~, xingMax] = max(xingTemp);
     [~, xingMin] = min(xingTemp);
     % 计算两条曲线的欧氏距离？就是点之间的间隔
-      wanDistArr(i) = abs(((wanMax - judgeMax) + (wanMin - judgeMin))/2);
-      xingDistArr(i) = abs(((xingMax - judgeMax) + (xingMin - judgeMin))/2);
-%      wanDistArr(i) = abs(wanMax - judgeMax);
-%      xingDistArr(i) = abs(xingMax - judgeMax);
+%       wanDistArr(i) = abs(((wanMax - judgeMax) + (wanMin - judgeMin))/2);
+%       xingDistArr(i) = abs(((xingMax - judgeMax) + (xingMin - judgeMin))/2);
+      wanDistArr(i) = abs(wanMax - judgeMaxWan);
+      xingDistArr(i) = abs(xingMax - judgeMaxXing);
     % 计算相应的Frechete Distance
-    wanFrecheteArr(i) = DiscreteFrechetDist(wanTemp, judgeTemp);
-    xingFrecheteArr(i) = DiscreteFrechetDist(xingTemp, judgeTemp);
+    wanFrecheteArr(i) = DiscreteFrechetDist(wanTemp, judgeTempWan);
+    xingFrecheteArr(i) = DiscreteFrechetDist(xingTemp, judgeTempXing);
 end
 %% 处理
 wanMatrix = [wanDistArr; wanFrecheteArr]';
@@ -71,9 +83,11 @@ ylabel('FDF')
 
 
 
-plot(data{1}(400:1200,13),data{17}(400:1200,9))
-hold on
-plot(data{1}(400:1200,13),data{17}(400:1200, 5))
-plot(data{1}(400:1200,13),data{17}(400:1200, 6))
-datetick('x','HH')
-legend('兴泰里','温度','日照')
+% plot(data{1}(400:1200,13),data{19}(400:1200,9))
+% hold on
+% judgeTempXing = -data{19}(400:1200, 11) + 2.5 * (data{19}(400:1200, 12) / 350);
+% plot(data{1}(400:1200,13), judgeTempXing)
+% %plot(data{1}(400:1200,13),data{9}(400:1200, 11))
+% %plot(data{1}(400:1200,13),data{9}(400:1200, 12))
+% datetick('x','HH')
+% legend('兴泰里','温度','日照')
